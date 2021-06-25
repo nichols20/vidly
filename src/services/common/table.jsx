2e9f943a7516261a32266c7375cf29aa80b59ed4
nichols20 <johnnytes10@gmail.com>
2021-06-25 12:51:25 -0400

    initialized table jsx file

---
+++ src/services/common/table.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+class Table extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Table;
