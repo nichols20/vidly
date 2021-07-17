64e61f38d33e071ec3353e11f5ecb7679e32abdf
nichols20 <johnnytes10@gmail.com>
2021-07-17 17:07:18 -0400

    learned about refs

--- src/services/common/loginform.jsx
+++ src/services/common/loginform.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import reactDom from 'react-dom';
 
 class LoginForm extends Component {
 
@@ -7,8 +8,21 @@ class LoginForm extends Component {
         paddingLeft: "2rem"
     }
 
+    username = React.createRef()
+
+
     handleSubmit = e => {
         e.preventDefault()
+
+        //const userName = document.getElementById('username').value
+        //this would be the solution to obtain the value of an input field in vanilla javascript, however this isn't the approach 
+        //you'd want to take using react jsx. In order to access the username element we would need to use a reference.
+        //to do this i first created a username object with the value React.createRef() this will create a ref object 
+        //after this I then defined the ref attribute to be the username property we have just defined above.
+        //so now instead of using the document. keyword you would do this
+        const username = this.username.current.value
+        //this ref has a property current which returns the dom element allowing us to access the value property.
+        //minimize the use of refs when it comes to building applications. 
 
         //call the server 
         console.log('submitted')
@@ -20,7 +34,7 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group mb-3" style={this.styles}>
                         <label htmlFor="username" className="form-label">Username</label>
-                        <input id='username' type="text" className="form-control" />
+                        <input autoFocus ref={this.username} id='username' type="text" className="form-control" />
                     </div>
                     <div className="form-group mb-3" style={this.styles}>
                         <label htmlFor="Password">Password</label>
